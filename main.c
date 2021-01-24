15ccd0c7f8934ced02a0a845af89054b2bae34d5
els0212@nate.com <hyi@c1r8s7.42seoul.kr>
2021-01-24 19:58:48 +0900

    coordinates final

--- main.c
+++ main.c
@@ -109,26 +109,33 @@ int main(int argc, char **argv)
 	for(int i = 0 ; i <compo->resolution->y; i++)
 		for(int j = 0 ; j < compo->resolution->x; j++)
 		{
-			double u = (1 - 2 * ((double)i + 0.5)) / (compo->resolution->y - 1);
-			double v = (2 * ((double)j + 0.5)) / (compo->resolution->x - 1) * compo->resolution->ratio * tan(compo->camera->fov / 2);
+			double u = (1 - 2 * ((double)i + 0.5) / (compo->resolution->y - 1)) * tan(compo->camera->fov / 2);
+			double v = (2 * ((double)j + 0.5) / (compo->resolution->x - 1) - 1) * compo->resolution->ratio * tan(compo->camera->fov / 2);
 			t_vector dir;
+			/*
 			double x = v * compo->camera->v->x + u * compo->camera->u->x + compo->camera->dir->x + compo->camera->vec->x;
 			double y = v * compo->camera->v->y + u * compo->camera->u->y + compo->camera->dir->y + compo->camera->vec->y;
 			double z = compo->camera->v->z + compo->camera->u->z + compo->camera->dir->z + compo->camera->vec->z;
 			ft_vector_init(&dir, x, y, z);
+			*/
+			//printf("u = %f v = %f\n", u, v);
+			dir = ft_vec_add(*compo->camera->vec, ft_vec_add(ft_vec_add(ft_vec_product_const(*compo->camera->u, u), ft_vec_product_const(*compo->camera->v, v)), *compo->camera->dir));
 			//printf("x = %f y = %f z = %f\n", x, y, z);
 			ray = ft_ray_init(compo->camera->vec, dir);
+			printf("dir x = %f y = %f z = %f\n", ray->dir->x, ray->dir->y, ray->dir->z);
 		//	ray = ft_ray_init(compo->camera->vec, ft_vec_add(compo->camera->ndc_left_bottom, ft_vec_add(ft_vec_product_const(compo->camera->ndc_horizon, v), ft_vec_product_const(compo->camera->ndc_vertical, u))));
 			//printf("ray dir x = %f y = %f z = %f\n", ray->dir->x, ray->dir->y, ray->dir->z);
 			t_color *temp_color = ft_ray_color(ray, compo->objects);
 			t_color ambient = ft_color_mult_const(compo->ambient->color, compo->ambient->ratio);
 			temp_color = ft_color_mult(temp_color, &ambient); 
+			/*
 			if (ray->ray_hit)
 			{
 				t_color shader = ft_shader(compo->light, compo->objects, *ray);
 				//printf("shader r = %d g = %d b = %d\n",shader.red, shader.green, shader.blue);
 				temp_color = ft_color_cpy(temp_color, ft_color_add(*temp_color, shader));
 			}
+			*/
 			//printf("r = %d, g = %d, b = %d\n", temp_color->red, temp_color->blue, temp_color->green);
 			//printf("amb r = %d g = %d b = %d\n",ambient.red, ambient.blue, ambient.green);
 			int col = (temp_color->red<<16) + (temp_color->green<<8) + (temp_color->blue<<0);
