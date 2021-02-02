c929a3654fa4085eb9d16a37cf48db870dfffbac
els0212@nate.com <hyi@c2r10s5.42seoul.kr>
2021-02-02 13:23:02 +0900

    ft_ray_hit_1 clear

--- ft_ray_hit_1.c
+++ ft_ray_hit_1.c
@@ -1,4 +1,5 @@
 #include "ft_ray_hit_1.h"
+
 /*
 ** oc = (A - C) vector
 ** a = t^2 * b dot b
@@ -35,7 +36,7 @@ int			ft_ray_hit_plane(t_object *plane, t_ray *ray, int t)
 {
 	double		denom;
 	t_vector	oc;
-	
+
 	denom = ft_dot_product(*plane->dir, *ray->dir);
 	if (fabs(denom) > EPSILON)
 	{
@@ -53,7 +54,7 @@ int			ft_ray_hit_square(t_object *square, t_ray *ray)
 	t_vector	oc;
 	t_vector	p;
 	double		t;
-	
+
 	denom = ft_dot_product(*square->dir, *ray->dir);
 	if (fabs(denom) > EPSILON)
 	{
@@ -75,7 +76,8 @@ int			ft_ray_hit_square(t_object *square, t_ray *ray)
 	return (0);
 }
 
-int			ft_triangle_inside_outside(t_vector p, t_vector a, t_vector b, t_vector n)
+int			ft_triangle_inside_outside(t_vector p,
+		t_vector a, t_vector b, t_vector n)
 {
 	t_vector	edge;
 	t_vector	p_edge;
@@ -89,29 +91,29 @@ int			ft_triangle_inside_outside(t_vector p, t_vector a, t_vector b, t_vector n)
 
 int			ft_ray_hit_triangle(t_object *triangle, t_ray *ray, int t)
 {
-	t_vector	a;
-	t_vector	b;
 	t_vector	p;
 	t_vector	n;
 	double		discriminant;
 
-	a = ft_vec_sub(*triangle->vec_second, *triangle->vec);
-	b = ft_vec_sub(*triangle->vec_third, *triangle->vec);
-	n = ft_normalize(ft_cross_product(a, b));
+	n = ft_normalize(ft_cross_product(
+				ft_vec_sub(*triangle->vec_second, *triangle->vec),
+				ft_vec_sub(*triangle->vec_third, *triangle->vec)));
 	if (fabs(discriminant = ft_dot_product(n, *ray->dir)) < EPSILON)
 		return (0);
 	if ((t = ft_dot_product(ft_vec_sub(*triangle->vec,
 						*ray->origin), n) / discriminant) < 0)
 		return (0);
 	p = ft_ray_at(*ray, t);
-	if (ft_triangle_inside_outside(p, *triangle->vec, *triangle->vec_second, n) == 0)
+	if (ft_triangle_inside_outside(p, *triangle->vec,
+				*triangle->vec_second, n) == 0)
 		return (0);
-	else if (ft_triangle_inside_outside(p, *triangle->vec_second, *triangle->vec_third, n) == 0)
+	else if (ft_triangle_inside_outside(p, *triangle->vec_second,
+				*triangle->vec_third, n) == 0)
 		return (0);
-	else if (ft_triangle_inside_outside(p, *triangle->vec_third, *triangle->vec, n) == 0)
+	else if (ft_triangle_inside_outside(p, *triangle->vec_third,
+				*triangle->vec, n) == 0)
 		return (0);
 	if (ft_ray_change_hit(ray, t, triangle) > 0)
 		return (1);
 	return (0);
 }
-
